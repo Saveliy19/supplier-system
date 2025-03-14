73823d5694742751b6ed8fbe69bc3ec65b76ca9a
Saveliy19 <91277989+Saveliy19@users.noreply.github.com>
2025-03-14 15:24:35 +0300

    First commit

---
+++ src/main/java/com/saveliy/suppliersystem/suppliersystem/SupplierSystemApplication.java
@@ -0,0 +1,13 @@
+package com.saveliy.suppliersystem.suppliersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupplierSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saveliy/suppliersystem/suppliersystem/SupplierSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saveliy.suppliersystem.suppliersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
