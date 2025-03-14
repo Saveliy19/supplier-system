617798ffa09875c4fe57cfb73b55844748376f8a
Saveliy19 <91277989+Saveliy19@users.noreply.github.com>
2025-03-14 15:57:38 +0300

    feat: создать начальные файлы контроллеров
    Созданы базовые файлы контроллеров для работы с поставщиками, продуктами, ценами, поставками и отчётами.

---
+++ src/main/java/com/saveliy/suppliersystem/suppliersystem/controller/DeliveryController.java
@@ -0,0 +1,9 @@
+package com.saveliy.suppliersystem.suppliersystem.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/deliveries")
+public class DeliveryController {
+}
