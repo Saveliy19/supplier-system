ab18d248c8069c5ee480e3338f658ac75e463a90
Saveliy19 <91277989+Saveliy19@users.noreply.github.com>
2025-03-16 21:56:40 +0300

    Обновлены конфигурации: добавлены комментарии в docker-compose.yml, обновлены тесты для PriceService и ReportService

--- src/test/java/com/saveliy/suppliersystem/service/price/PriceServiceImplementationTest.java
+++ src/test/java/com/saveliy/suppliersystem/service/price/PriceServiceImplementationTest.java
@@ -1,10 +1,17 @@
 package com.saveliy.suppliersystem.service.price;
 
+import com.saveliy.suppliersystem.dto.price.CreatePriceDTO;
+import com.saveliy.suppliersystem.dto.price.PriceDTO;
+import com.saveliy.suppliersystem.repository.PriceRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,18 +20,53 @@ import static org.junit.jupiter.api.Assertions.*;
 @ActiveProfiles("test")
 class PriceServiceImplementationTest {
 
+    private CreatePriceDTO priceDTO;
+
     @Autowired
     private PriceService priceService;
 
+
+    @BeforeEach
+    void setUp() {
+        priceDTO = new CreatePriceDTO(
+                100.0, LocalDate.now(), LocalDate.now().plusDays(2),
+                1, 6
+        );
+    }
+
     @Test
     void createPrice() {
+        PriceDTO price = priceService.createPrice(priceDTO);
+
+        assertNotNull(price);
+        assertEquals(price.productId(), priceDTO.productId());
+        assertEquals(price.price(), priceDTO.price());
+        assertEquals(price.startDate(), priceDTO.startDate());
+        assertEquals(price.endDate(), priceDTO.endDate());
     }
 
     @Test
     void getProductPricesByDate() {
+        List<PriceDTO> pricesBefore = priceService.getProductPricesByDate(6, LocalDate.now());
+
+        PriceDTO price = priceService.createPrice(priceDTO);
+
+        List<PriceDTO> pricesAfter = priceService.getProductPricesByDate(6, LocalDate.now());
+
+        assertNotNull(pricesAfter);
+        assertFalse(pricesAfter.isEmpty());
+        assertEquals(pricesAfter.size(), pricesBefore.size() + 1);
+
     }
 
     @Test
     void getSupplierPricesByDate() {
+        List<PriceDTO> pricesBefore = priceService.getSupplierPricesByDate(1, LocalDate.now());
+        PriceDTO price = priceService.createPrice(priceDTO);
+        List<PriceDTO> pricesAfter = priceService.getSupplierPricesByDate(1, LocalDate.now());
+
+        assertNotNull(pricesAfter);
+        assertFalse(pricesAfter.isEmpty());
+        assertEquals(pricesAfter.size(), pricesBefore.size() + 1);
     }
 }
--- src/test/java/com/saveliy/suppliersystem/service/report/ReportServiceImplementationTest.java
+++ src/test/java/com/saveliy/suppliersystem/service/report/ReportServiceImplementationTest.java
@@ -1,12 +1,69 @@
 package com.saveliy.suppliersystem.service.report;
 
+import com.saveliy.suppliersystem.dto.report.ReportDTO;
+import com.saveliy.suppliersystem.dto.report.SupplierReportDTO;
+import com.saveliy.suppliersystem.entity.dto.SupplierProductSummary;
+import com.saveliy.suppliersystem.repository.DeliveryRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class) // Используем расширение для Mockito
 class ReportServiceImplementationTest {
+
+    @Mock
+    private DeliveryRepository deliveryRepository; // Мокаем репозиторий
+
+    @InjectMocks
+    private ReportServiceImplementation reportService; // Сервис, который тестируем
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    @BeforeEach
+    void setUp() {
+        startDate = LocalDate.of(2025, 3, 1);
+        endDate = LocalDate.of(2025, 3, 31);
+    }
 
     @Test
     void getReport() {
+        List<SupplierProductSummary> testData = List.of(
+                new SupplierProductSummary(1, "Supplier A", 1, "Product X", 100.0, 200.0),
+                new SupplierProductSummary(1, "Supplier A", 2, "Product Y", 50.0, 100.0),
+                new SupplierProductSummary(2, "Supplier B", 3, "Product Z", 200.0, 400.0)
+        );
+
+        when(deliveryRepository.getSupplierProductWeightsAndCosts(startDate, endDate)).thenReturn(testData);
+
+        ReportDTO report = reportService.getReport(startDate, endDate);
+
+        assertNotNull(report);
+        assertEquals(startDate, report.getStartDate());
+        assertEquals(endDate, report.getEndDate());
+        assertEquals(2, report.getSuppliers().size());
+
+        SupplierReportDTO supplierA = report.getSuppliers().get(0);
+        assertEquals(1, supplierA.getId());
+        assertEquals("Supplier A", supplierA.getName());
+        assertEquals(150.0, supplierA.getTotalWeight());
+        assertEquals(300.0, supplierA.getTotalPrice());
+        assertEquals(2, supplierA.getProducts().size());
+
+        SupplierReportDTO supplierB = report.getSuppliers().get(1);
+        assertEquals(2, supplierB.getId());
+        assertEquals("Supplier B", supplierB.getName());
+        assertEquals(200.0, supplierB.getTotalWeight());
+        assertEquals(400.0, supplierB.getTotalPrice());
+        assertEquals(1, supplierB.getProducts().size());
     }
 }
